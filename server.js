4f2b83d00953fc07356b673d87a2f2fc640f45a1
DarkCode <test@fake.com>
2021-10-04 23:14:51 -0500

    first commmit

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/PRUEBA'));
+app.use(express.static('./dist/prueba'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/PRUEBA/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/prueba/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
