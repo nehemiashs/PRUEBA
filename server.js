165c0c75553581e11163f205f90619dea7338f78
DarkCode <test@fake.com>
2021-10-04 23:34:35 -0500

    first

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/prueba'));
+app.use(express.static('./dist/PRUEBA'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/prueba/'}),
+    res.sendFile('index.html', {root: 'dist/PRUEBA/'}),
 );
 
 // Start the app by listening on the default Heroku port
